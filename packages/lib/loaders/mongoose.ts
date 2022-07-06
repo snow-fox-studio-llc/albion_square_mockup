4e79c4a94bb97ec89644d6e2f42e0c06dc4d1bdf
Luis Lopez Cardona <22754517+luis815@users.noreply.github.com>
2022-07-06 13:12:41 +0000

    Mongoose connection refactor

--- packages/lib/loaders/mongoose.ts
+++ packages/lib/loaders/mongoose.ts
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-const DEFAULT_MONGO_URL: string = "mongodb://localhost:27017/albion_square";
+export default async (extraMongoUrls: string[]) => {
+	await mongoose.connect(
+		process.env.MONGO_URL || "mongodb://localhost:27017/albion_square"
+	);
 
-export default async (additionalMongoUrls: string[]) => {
-	await mongoose.connect(process.env.MONGO_URL || DEFAULT_MONGO_URL);
-
-	for (const additionalMongoUrl of additionalMongoUrls) {
-		await mongoose.connect(additionalMongoUrl);
+	for (const extraMongoUrl of extraMongoUrls) {
+		await mongoose.connect(extraMongoUrl);
 	}
 };
