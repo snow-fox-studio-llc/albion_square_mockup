8c2120fda249b7fc641b80d940a646ee8d5b42c4
Luis Lopez Cardona <22754517+luis815@users.noreply.github.com>
2022-07-19 13:47:47 -0400

    Simplified mongoose connections

--- packages/lib/loaders/mongoose.ts
+++ packages/lib/loaders/mongoose.ts
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-export default async (extraMongoUrls: string[]) => {
+export default async () => {
 	await mongoose.connect(
 		process.env.MONGO_URL || "mongodb://localhost:27017/albion_square"
 	);
 
-	for (const extraMongoUrl of extraMongoUrls) {
-		await mongoose.connect(extraMongoUrl);
+	if(process.env.MONGO_URL_ALT) {
+		await mongoose.createConnection(process.env.MONGO_URL_ALT).asPromise();
 	}
 };
