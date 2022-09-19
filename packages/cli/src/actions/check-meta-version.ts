d97dbb8b10f5b08b58d5f32a654264378686cc2b
Luis Lopez Cardona <22754517+luis815@users.noreply.github.com>
2022-09-19 15:20:28 -0400

    Added check-meta-version function and wrapper

--- packages/cli/src/actions/check-meta-version.ts
+++ packages/cli/src/actions/check-meta-version.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 import cliRecords from "@as/models/cli-records";
 
-export default async () => {
+export interface MetaVersionStatus {
+	upToDate: boolean;
+	updateHash?: string;
+}
+
+export const getMetaVersionStatus = async (): Promise<MetaVersionStatus> => {
 	console.log("Getting albion online metadata version");
 	const metaLatestCommitHash = (
 		await axios.get(
@@ -14,7 +19,17 @@ export default async () => {
 
 	const cliRecord = await cliRecords.findVersion();
 
-	console.log(`Version: ${cliRecord.version}`);
+	console.log(`CLI Record: ${JSON.stringify({ version: cliRecord.version })}`);
 
-	cliRecords.create(metaLatestCommitHash);
+	const upToDate = metaLatestCommitHash === cliRecord.version;
+
+	if (upToDate) {
+		return { upToDate };
+	} else {
+		return { upToDate, updateHash: metaLatestCommitHash };
+	}
+};
+
+export default async (): Promise<void> => {
+	await getMetaVersionStatus();
 };
